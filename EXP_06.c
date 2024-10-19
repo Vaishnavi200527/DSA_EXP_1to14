22d4ec4ff74e81a8940d0c92fa9a1e455bd154e7
Vaishnavi200527 <2023.vaishnavi.kumbhar@ves.ac.in>
2024-10-19 10:02:06 -0700

    Update EXP_06.c

--- EXP_06.c
+++ EXP_06.c
@@ -1,51 +1,85 @@
-#include <stdio.h>  
-  
-int max_node = 15;  
-char tree[] = {'\0', 'D', 'A', 'F', 'E', 'B', 'R', 'T', 'G', 'Q', '\0', '\0', 'V', '\0', 'J', 'L'};  
-  
-int get_right_child(int index){  
-  if (tree[index] != '\0' && ((2 * index) + 1) <= max_node)  
-    return (2 * index) + 1;  
-  return -1;  
-}  
-  
-int get_left_child(int index){  
-  if (tree[index] != '\0' && (2 * index) <= max_node)  
-    return 2 * index;  
-  return -1;  
-}  
-  
-void preorder(int index){  
-  if (index > 0 && tree[index] != '\0'){  
-    printf(" %c ", tree[index]);  
-    preorder(get_left_child(index));  
-    preorder(get_right_child(index));  
-  }  
-}  
-  
-void postorder(int index){  
-  if (index > 0 && tree[index] != '\0'){  
-    postorder(get_left_child(index));  
-    postorder(get_right_child(index));  
-    printf(" %c ", tree[index]);  
-  }  
-}  
-  
-void inorder(int index){  
-  if (index > 0 && tree[index] != '\0'){  
-    inorder(get_left_child(index));  
-    printf(" %c ", tree[index]);  
-    inorder(get_right_child(index));  
-  }  
-}  
-  
-int main() {  
-  printf("Preorder:\n");  
-  preorder(1);  
-  printf("\nPostorder:\n");  
-  postorder(1);  
-  printf("\nInorder:\n");  
-  inorder(1);  
-  printf("\n");  
-  return 0;  
-} 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Node structure for linked list stack
+struct Node {
+    char data;
+    struct Node* next;
+};
+
+// Function to push an element onto the stack
+void push(struct Node** top, char data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *top;
+    *top = newNode;
+}
+
+// Function to pop an element from the stack
+char pop(struct Node** top) {
+    if (*top == NULL) {
+        return '\0'; // Return a null character if the stack is empty
+    }
+    struct Node* temp = *top;
+    char popped = temp->data;
+    *top = (*top)->next;
+    free(temp);
+    return popped;
+}
+
+// Function to get the top element of the stack without removing it
+char peek(struct Node* top) {
+    return (top != NULL) ? top->data : '\0';
+}
+
+// Function to check if the stack is empty
+int isEmpty(struct Node* top) {
+    return top == NULL;
+}
+
+// Function to perform "READ" operation
+void READ(struct Node* Undo) {
+    struct Node* temp = Undo;
+    // Print the Undo stack elements
+    while (temp != NULL) {
+        printf("%c", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Function to perform the queries on the document
+void QUERY(char Q[][10], int N) {
+    struct Node* Undo = NULL; // Undo stack
+    struct Node* Redo = NULL; // Redo stack
+
+    // Traverse all the queries
+    for (int i = 0; i < N; i++) {
+        if (strcmp(Q[i], "UNDO") == 0) {
+            char X = pop(&Undo); // Perform UNDO operation
+            if (X != '\0') {
+                push(&Redo, X); // Push to REDO stack
+            }
+        } else if (strcmp(Q[i], "REDO") == 0) {
+            char X = pop(&Redo); // Perform REDO operation
+            if (X != '\0') {
+                push(&Undo, X); // Push to UNDO stack
+            }
+        } else if (strcmp(Q[i], "READ") == 0) {
+            READ(Undo); // Perform READ operation
+        } else {
+            // WRITE operation
+            char character = Q[i][6]; // Get the character after "WRITE "
+            push(&Undo, character); // Push character to Undo stack
+        }
+    }
+}
+
+// Driver Code
+int main() {
+    char Q[][10] = { "WRITE A", "WRITE B", "WRITE C", "UNDO", "READ", "REDO", "READ" };
+    int N = sizeof(Q) / sizeof(Q[0]); // Total count of queries
+    QUERY(Q, N); // Pass both arguments to QUERY
+    return 0;
+}
